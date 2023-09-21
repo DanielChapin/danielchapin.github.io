67c1bcfb94bc105d73ceffda93385764f36a2c63
Daniel Chapin <dchapes123@gmail.com>
2023-09-21 13:02:37 -0400

    Remove test link

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,8 @@
-import { Link } from "react-router-dom";
 import Terminal from "../elements/terminal/Terminal";
 
 const App = () => {
   return (
     <div className="w-screen h-screen">
-      <Link to={"/test"}>Link to test page</Link>
       <Terminal />
     </div>
   );
